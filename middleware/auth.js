10d66000946da9d8241415130ceb099e5ba202de
delean <dennishockey19@yahoo.de>
2020-07-09 11:42:07 +0200

    jwt auth

---
+++ middleware/auth.js
@@ -0,0 +1,21 @@
+const jwt = require("jsonwebtoken");
+const config = require("config");
+
+module.exports = function(req, res, next) {
+    //Token vom header holen
+    const token = req.header("x-auth-token");
+    //private Route und kein Token vorhanden
+    if(!token) {
+        return res.status(401).json({ msg: "Kein Token, Authentifizierung fehlgeschlagen"});
+    }
+
+    try {
+        const decoded = jwt.verify(token, config.get("jwtSecret"));
+
+        req.user = decoded.user;
+        next();
+    //Token aber nicht gültig
+    } catch(err) {
+        res.status(401).json({ msg: "Token ist nicht gültig"});
+    }
+}
--- routes/api/users.js
+++ routes/api/users.js
@@ -2,6 +2,8 @@ const express = require("express");
 const router = express.Router();
 const { check, validationResult } = require("express-validator");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const config = require("config");
 const inputChecks = [
     check("name", "Name wird benötigt ").not().isEmpty(),
     check("email", "Bitte geben Sie eine gültige Email ein.").isEmail(),
@@ -40,7 +42,20 @@ router.post("/", inputChecks, async(req,res) => {
 
         await user.save();
 
-        res.send("Nutzer registriert");
+        const payload = {
+            user: {
+                id: user.id
+            }
+        }
+
+        jwt.sign(payload,
+            config.get("jwtSecret"),
+            { expiresIn: 360000 },
+            (err, token) => { 
+                if (err) throw err;
+                res.json({ token })
+             }
+            );
     }
     catch(err) {
         console.error(err.message);
