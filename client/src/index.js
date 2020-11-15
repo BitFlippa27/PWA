5953890ef346fc525d5aaf3a870b9f948b8d8132
delean <dennishockey19@yahoo.de>
2020-11-15 14:34:46 +0100

    serviceWorker runs

--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+
 
 
 ReactDOM.render(
@@ -11,8 +11,33 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
+/*
+if("serviceWorker" in  navigator){
+  initServiceWorker();
+}
+async function initServiceWorker() {
+    try {
+      var reg = await navigator.serviceWorker.register("/sw.js", {
+        updateViaCache: "none"
+      });
+      console.log("Registriert!")
+    }
+    catch(err) {
+      console.error(err);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+    }
+
+  }
+*/
+if("serviceWorker" in navigator) {
+  initServiceWorker();
+}
+async function initServiceWorker() {
+  try {
+    await navigator.serviceWorker.register("serviceWorker.js");
+    console.log("registriert!!!")
+  }
+  catch(err) {
+    console.error(err);
+  }
+}
