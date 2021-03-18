6e58c2ec407b9e3af3c41069d317329af921484a
DeLean <dennishockey19@yahoo.de>
2021-03-18 14:25:43 +0100

    non graphql version

--- client/src/service-worker.js
+++ client/src/service-worker.js
@@ -1,10 +1,3 @@
-// This service worker can be customized!
-// See https://developers.google.com/web/tools/workbox/modules
-// for the list of available Workbox modules, or add any other
-// code you'd like.
-// You can also remove this file if you'd prefer not to use a
-// service worker, and the Workbox build step will be skipped.
-//importScripts("dexie.js");
 import { setCacheNameDetails } from 'workbox-core';
 import { ExpirationPlugin } from 'workbox-expiration';
 import { precacheAndRoute, matchPrecache } from 'workbox-precaching';
